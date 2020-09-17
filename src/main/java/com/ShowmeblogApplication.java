5b7368c57463bab7a29a478b01d0f4147df53b95
YCHEN2323 <837849938@qq.com>
2020-09-17 21:37:31 +0800

    第一次基础框架搭建好，日志部分配置完成，AOP切面log完成

---
+++ src/main/java/com/ShowmeblogApplication.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowmeblogApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ShowmeblogApplication.class, args);
+    }
+
+}
